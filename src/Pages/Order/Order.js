dcdb38215fbdfae4873a892278259fb2362376e6
Programming Hero <programming.hero1@gmail.com>
2022-04-23 00:09:14 -0500

    load orders for email

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Order = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([]);
+    useEffect( () => {
+        
+        const getOrders = async() =>{
+            const email = user.email;
+            const url = `http://localhost:5000/order?email=${email}`;
+            const {data} = await axios.get(url);
+            setOrders(data);
+        }
+        getOrders();
+
+    }, [user])
     return (
         <div>
-            <h2>Your orders</h2>
+            <h2>Your orders: {orders.length}</h2>
+
         </div>
     );
 };
